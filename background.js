b23c89cae1955a1f49618717777dfb4f1be8b57d
Shubham Sharma <cossth@gmail.com>
2020-05-16 16:25:42 +0530

    ID is important, Add name

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 var sendPushNotification = function (data) {
   var content = JSON.stringify(data);
   $.ajax({
-    url: "http://localhost:9200/whatsapp/update/" + data.id,
+    url: "http://localhost:9200/whatsapp/update/" + newGuid(),
     method: "PUT",
     dataType: "json",
     crossDomain: true,
@@ -10,6 +10,13 @@ var sendPushNotification = function (data) {
     cache: false,
   });
 };
+function newGuid() {
+  function _p8(s) {
+    var p = (Math.random().toString(16) + "000000000").substr(2, 8);
+    return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
+  }
+  return _p8() + _p8(true) + _p8(true) + _p8();
+}
 chrome.runtime.onConnect.addListener(function (port) {
   if (port.name === "elastic") {
     port.onMessage.addListener(function (data) {
--- pagescript.js
+++ pagescript.js
@@ -1,9 +1,9 @@
-var interval = setInterval(function () {
+var clear = setInterval(function () {
   if (document.head != null) {
-    clearInterval(interval);
-    var element = document.createElement("script");
-    element.innerHTML = `var WebSocketProxy=new Proxy(window.WebSocket,{construct:function(e,t){const n=new e(...t),a=e=>{},s=e=>{if(e&&e.data&&e.data.indexOf&&-1!==e.data.indexOf("Presence")){var t=e.data.substr(e.data.indexOf(",")+1,e.data.length-1);if(t){var n=JSON.parse(t)[1],a={data:{number:n.id.split("@")[0],presence:n.type,date:""+Date.now()}};n.t&&(a.data.last=n.t),window.postMessage(a,"*")}}},r=e=>{n.removeEventListener("open",a),n.removeEventListener("message",s),n.removeEventListener("close",r)};return n.addEventListener("open",a),n.addEventListener("message",s),n.addEventListener("close",r),n}});window.WebSocket=WebSocketProxy;`;
-    document.body.prepend(element);
+    clearInterval(clear);
+    var s = document.createElement("script");
+    s.innerHTML = `var WebSocketProxy=new Proxy(window.WebSocket,{construct:function(e,t){const n=new e(...t),a=()=>{},s=e=>{if(e&&e.data){var t=e.data.substr(e.data.indexOf(",")+1,e.data.length-1);if(t){var n=JSON.parse(t)[1],a={data:{number:n.id.split("@")[0],presence:n.type,date:""+Date.now(),name:""+n.id.split("@")[0],last:n.t}};window.postMessage(a,"*")}}},o=()=>{n.removeEventListener("open",a),n.removeEventListener("message",s),n.removeEventListener("close",o)};return n.addEventListener("open",a),n.addEventListener("message",s),n.addEventListener("close",o),n}});window.WebSocket=WebSocketProxy;`;
+    document.body.prepend(s);
   }
 }, 1);
 
